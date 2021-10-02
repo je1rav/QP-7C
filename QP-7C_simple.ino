d3e233932cce682f4de95c7e3c6d238873c5c3b9
je1rav <91771666+je1rav@users.noreply.github.com>
2021-10-02 20:08:46 +0900

    Update QP-7C_simple.ino

--- QP-7C_simple.ino
+++ QP-7C_simple.ino
@@ -7,7 +7,7 @@ void setup(void)
 {
   //si5351の準備 ここから  
   word cal_factor = 0;
-  si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, cal_factor); 
+  si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, 0); 
   si5351.set_correction(cal_factor, SI5351_PLL_INPUT_XO);
  
   si5351.set_pll(SI5351_PLL_FIXED, SI5351_PLLA);
