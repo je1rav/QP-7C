f84579a816d10b4079bd81b2ee86b6e22bfc444f
je1rav <91771666+je1rav@users.noreply.github.com>
2021-10-02 20:05:10 +0900

    Update QP-7C_simple.ino

--- QP-7C_simple.ino
+++ QP-7C_simple.ino
@@ -6,7 +6,7 @@ long int freq = 7010000;; // freqency in Hz
 void setup(void)
 {
   //si5351の準備 ここから  
-  word cal_factor = 32392;
+  word cal_factor = 0;
   si5351.init(SI5351_CRYSTAL_LOAD_8PF, 0, cal_factor); 
   si5351.set_correction(cal_factor, SI5351_PLL_INPUT_XO);
  
